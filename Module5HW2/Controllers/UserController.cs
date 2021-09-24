39528fa49c9620f7d6914cf4a2829868ff21fd07
sofia_radul <sofiiar@wizardsdev.com>
2021-09-24 23:21:32 +0300

    add form ui/back table logic

---
+++ Module5HW2/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Module5HW2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module5HW2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public string Register([FromForm] User user)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(user));
+            return "HW!";
+        }
+    }
+}
+
---
+++ Module5HW2/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Module5HW2.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
